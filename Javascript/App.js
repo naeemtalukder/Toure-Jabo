804fe91b71c926967e339e9d8978cfe7702b216f
naeemtalukder <naeemtalukder169135@gmail.com>
2023-02-28 23:07:46 +0600

    card add

---
+++ Javascript/App.js
@@ -0,0 +1,7 @@
+var carObject = {
+    vehicle: "Car",
+    imageUrl:"https://media.istockphoto.com/id/1372188593/photo/modern-city-bus.jpg?b=1&s=170667a&w=0&k=20&c=-2D1HiCF5NCus9CWUnziiTeKD_x8n1tmiZfJJ_GDzXk=",
+    farePerKilo: 3,
+    capacity: 4,
+    description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis ut accusantium saepe provident repudiandae, placeat voluptas similique delectus blanditiis nam!"
+};
